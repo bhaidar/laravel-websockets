43ad04024540e99f210e58bc54d91d76c2aabd56
Bilal <bhaidar@gmail.com>
2023-07-04 21:54:46 +0300

    server-side packages installed

---
+++ app/Events/PusherStatusTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PusherStatusTest implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('pusher-status'),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            "Testing Pusher connection from inside Laravel",
+        ];
+    }
+}
